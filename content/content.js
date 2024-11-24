9f0d4626b9ba0302521349c0cbd994b3190b0088
sohag-arnifi <sohag.b@arnifi.com>
2024-11-25 03:18:35 +0600

    automatic modal close function added

--- content/content.js
+++ content/content.js
@@ -6,24 +6,19 @@ import { store } from "../src/redux/store"; //noticeModal
 
 __webpack_public_path__ = chrome.runtime.getURL("dist/");
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const button = document.getElementById("emergencyNoticeCloseBtn");
-//   const modalContainer = document.getElementById("noticeModal");
-//   console.log(modalContainer);
-//   if (button) {
-//     button.click(); // Click the button
-//     console.log("Emergency notice button clicked.");
-//   } else {
-//     console.warn("Button not found.");
-//   }
-// });
-
 const div = document.createElement("div");
 div.id = "react-root";
 document.body.appendChild(div);
 
-// const button = document.getElementById("emergencyNoticeCloseBtn");
-// console.log(button);
+setTimeout(() => {
+  const emergencyNoticeCloseBtn = document.getElementById(
+    "emergencyNoticeCloseBtn"
+  );
+
+  if (emergencyNoticeCloseBtn) {
+    emergencyNoticeCloseBtn.click();
+  }
+}, 500);
 
 chrome.storage.local.get((result) => {
   if (result?.logData?.success && result?.logData?.data?.token) {
